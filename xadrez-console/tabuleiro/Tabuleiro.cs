c57ba4380e659428f6e50d699720ba7de8f47ad5
vittor <vittordemelo@gmail.com>
2022-05-21 15:19:50 -0300

    Exception
    Tratando alguns erros

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -12,22 +12,48 @@ namespace tabuleiro
         public int colunas { get; set; }
         private Peca[,] pecas;
 
-        public Tabuleiro( int linhas, int colunas)
+        public Tabuleiro(int linhas, int colunas)
         {
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
 
-        public Peca peca (int linha, int coluna)
+        public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
         }
-
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool possicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= linhas || pos.Coluna < 0 || pos.Coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao pos)
+        {
+            if (!possicaoValida(pos))
+                throw new TabuleiroException("Posição Invalida");
         }
     }
 }
