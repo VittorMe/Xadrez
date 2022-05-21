b0b24b6da77f3b46b0c0080515e329b2afa0cce9
vittor <vittordemelo@gmail.com>
2022-05-21 19:11:02 -0300

    Pecas
    Controlando as peças capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,15 +12,12 @@ namespace xadrez_console
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                while (!partida.terminada)
+                while (!partida.Terminada)
                 {
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
 
                         Console.WriteLine();
